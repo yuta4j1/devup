cc7243cd3a732bebe19ec9027a50e21c507ba6c4
yuta4j1 <kascado.ys10@gmail.com>
2019-05-27 23:34:32 +0900

    feat: 'git add' 'git commit'

--- gitopt/git_operation.go
+++ gitopt/git_operation.go
@@ -2,11 +2,26 @@ package gitopt
 
 import (
 	"fmt"
-	"gopkg.in/src-d/go-git.v4"
+	. "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
-func GitInit(path string) error {
+// git init operation
+func GitInit(path string) (*Repository, error) {
 	fmt.Println("[git init]")
-	_, err := git.PlainInit(path, true)
+	repo, err := PlainInit(path, false)
+	return repo, err
+}
+
+// git add operation
+func GitAdd(w *Worktree, glob string) error {
+	fmt.Println("[git Add]")
+	err := w.AddGlob(glob)
 	return err
+}
+
+// git commit operation
+func GitCommit(wt *Worktree, msg string, opt *CommitOptions) (plumbing.Hash, error) {
+	ph, err := wt.Commit(msg, opt)
+	return ph, err
 }
