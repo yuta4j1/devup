24940d60a12b1d1220d000cb875b5b9625020ac0
yuta4j1 <kascado.ys10@gmail.com>
2019-05-27 23:39:02 +0900

    feat: 'git add', 'git commit', and fetch access token

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"fmt"
 	"os"
+	"time"
 	"log"
-	cli "gopkg.in/urfave/cli.v1"
+	"gopkg.in/urfave/cli.v1"
+	. "gopkg.in/src-d/go-git.v4"
+	. "gopkg.in/src-d/go-git.v4/plumbing"
 	"./gitopt"
 )
 
@@ -23,17 +28,49 @@ func main() {
 		// default path is current directory
 		// getPath := currentDir()
 		// テスト用
-		getPath := "C:\\Users\\kasca\\OneDrive\\ドキュメント\\sandbox-Go\\devup-test"
+		getPath := "C:\\Users\\kasca\\OneDrive\\ドキュメント\\git-test\\srctest2"
 		if c.NArg() > 0 {
 			getPath = c.Args()[0]
 		}
 		log.Println("getPath: ", getPath)
 		// git init
-		err := gitopt.GitInit(getPath)
+		repo, err := gitopt.GitInit(getPath)
+		if err != nil {
+			log.Fatal(err)
+			// if target repository is already initialized, advance the process regardless.
+			// return nil
+		}
+		// get Worktree
+		workTree, err := repo.Worktree()
 		if err != nil {
 			log.Fatal(err)
 			return nil
 		}
+		// TODO .gitignoreで指定したファイルを除外できているか動作確認
+		addErr := gitopt.GitAdd(workTree, ".")
+		if addErr != nil {
+			log.Fatal(err)
+		}
+
+		hash, err := gitopt.GitCommit(workTree, "first commit", &CommitOptions{
+			All: true,
+			Author: &object.Signature{
+				Name: "yuta4j1",
+				Email: "kascado.ys10@gmail.com",
+				When: time.Now(),
+			},
+		})
+		fmt.Println(hash)
+		// アクセストークンを取得する
+		gitopt.FetchAccessToken()
+		// プロジェクト名から、同名リポジトリがリモートリポジトリに存在するかをチェックする
+		// 存在する場合、リモートリポジトリに既存プロジェクトが存在するため、
+		// 処理の続行 or 中断を選択するコマンドを表示する
+
+		// 同名リポジトリがない場合、リモートリポジトリを作成し、git pushする。
+
+		
+
 		// TODO git commit
 		// TODO create remote repository
 		
