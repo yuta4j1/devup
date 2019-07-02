026be53d3b5d6a896fba549888c046d2c87edca8
yuta4j1 <kascado.ys10@gmail.com>
2019-07-02 21:44:07 +0900

    fix: refactor

--- gitopt/github_operation.go
+++ gitopt/github_operation.go
@@ -1,52 +1,10 @@
 package gitopt
 
 import (
-	"fmt"
-	"log"
 	"context"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
-	api "../api"
-	. "../api/types"
 )
-
-func FetchAccessToken() (string, error) {
-	// initialize response json data type
-	authResponse := AuthorizationsResponse{
-		Id: "",
-		Url: "",
-		Scopes: []string{""},
-		Token: "",
-		TokenLastEight: "",
-		HashedToken: "",
-		App: AppStruct{
-			Url: "",
-			Name: "",
-			ClientId: "",
-		},
-		Note: "",
-		NoteUrl: "",
-		UpdatedAt: "",
-		CreatedAt: "",
-		Fingerprint: "",
-	}
-	// post
-	out, err := api.Post("https://api.github.com/authorizations", AccessTokenParam{
-		Scopes: []string{"repo"},
-		Note: "get access token",
-	}, authResponse)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	v, ok := out.(AuthorizationsResponse)
-	if ok {
-		fmt.Println(v)
-	}
-
-	return "", nil
-
-}
 
 func InitClient(accessToken string) (*github.Client, context.Context) {
 	ctx := context.Background()
