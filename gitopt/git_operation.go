d89b71405ddf2fa00c49a5033eb8b2b66db7d218
yuta4j1 <kascado.ys10@gmail.com>
2019-07-30 20:03:47 +0900

    fix: mask user password, modify comment

--- gitopt/git_operation.go
+++ gitopt/git_operation.go
@@ -1,7 +1,6 @@
 package gitopt
 
 import (
-	"fmt"
 	"context"
 	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
@@ -11,28 +10,24 @@ import (
 
 // git init operation
 func GitInit(path string) (*git.Repository, error) {
-	fmt.Println("[git init]")
 	repo, err := git.PlainInit(path, false)
 	return repo, err
 }
 
 // git add operation
 func GitAdd(w *git.Worktree, glob string) error {
-	fmt.Println("[git Add]")
 	err := w.AddGlob(glob)
 	return err
 }
 
 // git commit operation
 func GitCommit(wt *git.Worktree, msg string, opt *git.CommitOptions) (plumbing.Hash, error) {
-	fmt.Println("[git commit]")
 	ph, err := wt.Commit(msg, opt)
 	return ph, err
 }
 
 // git 'remote add' operation
 func GitCreateRemote(r git.Repository, cloneUrl string) (*git.Remote, error) {
-	fmt.Println("[cloneUrl]", cloneUrl)
 	remote, err := r.CreateRemote(&config.RemoteConfig{
 		Name: "origin",
 		URLs: []string{cloneUrl},
--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"os"
 	"time"
 	"log"
+	"syscall"
+    "golang.org/x/crypto/ssh/terminal"
 	"github.com/c-bata/go-prompt"
 	"github.com/motemen/go-gitconfig"
 	"gopkg.in/urfave/cli.v1"
 	. "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 	"github.com/yuta4j1/devup/gitopt"
+	"github.com/kyokomi/emoji"
 )
 
 type User struct {
@@ -45,6 +48,7 @@ func main() {
 			targetPath = currentDir()
 		}
 		projName := projectName(targetPath)
+		fmt.Println("Project path to be initialized: ", targetPath)
 		// git init
 		repo, err := gitopt.GitInit(targetPath)
 		if err != nil {
@@ -52,24 +56,23 @@ func main() {
 			// if target repository is already initialized, advance the process regardless.
 			// return nil
 		}
+		fmt.Println("✔ git init")
 		// get Worktree
 		workTree, err := repo.Worktree()
 		if err != nil {
 			log.Fatal(err)
 			return nil
 		}
-		// TODO .gitignoreで指定したファイルを除外できているか動作確認
 		addErr := gitopt.GitAdd(workTree, ".")
 		if addErr != nil {
 			log.Fatal(err)
 		}
+		fmt.Println("✔ git Add")
 		// git commit
 		// don't use hash at this point
 		// get git-config info
 		var config User
 		err = gitconfig.Load(&config)
-		fmt.Println("[giyconfig] UserName", config.UserName)
-		fmt.Println("[giyconfig] UserName", config.Email)
 		_, err = gitopt.GitCommit(workTree, "first commit", &CommitOptions{
 			All: true,
 			Author: &object.Signature{
@@ -78,14 +81,17 @@ func main() {
 				When: time.Now(),
 			},
 		})
+		fmt.Println("✔ git Commit")
 		// Initialize github client object
 		githubClient, ctx := gitopt.InitClient(accessToken)
-		user, _, err := githubClient.Users.Get(ctx, config.UserName)
-		fmt.Println("[URL]", *user.URL)
-		fmt.Println("[URL]", *user.ReposURL)
 
 		// TODO validation access token
 		repos, _, err := githubClient.Repositories.List(ctx, "", nil)
+		// get account name
+		var accountName string
+		if len(repos) != 0 {
+			accountName = *repos[0].Owner.Login
+		}
 		// verify whether there is a project with the same name as local repository in the remote repository
 		for _, repo := range repos {
 			if projName == *repo.Name {
@@ -96,37 +102,46 @@ func main() {
 
 		// create new project at remote repository
 		newRepo, _, err := gitopt.GithubCreateRepository(githubClient, ctx, projName)
-		fmt.Println("[created remote repository!]")
-		fmt.Println("repo ID: ", *newRepo.ID)
-		fmt.Println("repo FullName: ", *newRepo.FullName)
-		fmt.Println("repo CreatedAt: ", *newRepo.CreatedAt)
-		fmt.Println("repo CloneURL: ", *newRepo.CloneURL)
+		fmt.Println("✔ created remote repository")
+		fmt.Println("  ID: ", *newRepo.ID)
+		fmt.Println("  FullName: ", *newRepo.FullName)
+		fmt.Println("  CreatedAt: ", *newRepo.CreatedAt)
+		fmt.Println("  CloneURL: ", *newRepo.CloneURL)
 		
 
 		// git remote add
-		remoteRepo, err := gitopt.GitCreateRemote(*repo, "https://github.com/yuta4j1" + projName + ".git")
+		remoteRepo, err := gitopt.GitCreateRemote(*repo, "https://github.com/" + accountName + "/" + projName + ".git")
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Println("Please input your username.")
+		fmt.Println("Please input your GitHub account.")
+		fmt.Println("Username: ")
 		userName := prompt.Input(": ", makeCompleter(
 			prompt.Suggest{Text: "username", Description: "your username for authentication"}))
-		fmt.Println("Please input your password.")
+		fmt.Println("Password: ")
 		// TODO mask password input
-		password := prompt.Input(": ", makeCompleter(
-			prompt.Suggest{Text: "password", Description: "your passwords for authentication"}))
-
+		// password := prompt.Input(": ", makeCompleter(
+		// 	prompt.Suggest{Text: "password", Description: "your passwords for authentication"}))
+		password, err := terminal.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			log.Fatal(err)
+		}
 		// create 'master' branch
 		err = gitopt.GitCreateBranch(*repo)
 		if err != nil {
 			log.Fatal(err)
 		}
+		fmt.Println("✔ created 'master' branch")
 		// git push
-		err = gitopt.GitPush(remoteRepo, ctx, userName, password)
+		err = gitopt.GitPush(remoteRepo, ctx, userName, string(password))
 		if err != nil {
 			log.Fatal("[git push]", err)
-		}	
+		}
+		fmt.Println("✔ git Push")
+
+		fmt.Print("Done successfully!")
+		emoji.Println(":tada:")
 
 		return nil
 	}
